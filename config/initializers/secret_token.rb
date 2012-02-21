9e0625dd11a5f0ee91e3be529b52a345dfb1a692
박성우 <sungwpark@ebay.com>
2012-02-21 13:21:47 +0900

    renewal init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pdfv5Admin::Application.config.secret_token = '4f1ffcca9c326c06e39fd455f49ec86590f991303af98f7fe8b7afc8789bde3cb4f0bf05143252a723bd37b9d4342a1c0cc713a5065b632ac6074c33c0985617'
