9f09e37d1531e7712db827869cf5c208c72a562a
박성우 <sungwpark@ebay.com>
2012-03-13 11:27:46 +0900

    아직 사용하지 않는 histories_observer comment out

--- app/models/history_observer.rb
+++ app/models/history_observer.rb
@@ -1,13 +1,13 @@
 class HistoryObserver < ActiveRecord::Observer
   def after_update(history)
-    if history.result.eql? "failure"
+    #if history.result.eql? "failure"
       #Rails.logger.info "job 처리실패 : #{history.job.name}"
       #job 실패
-    end
+    #end
     
-    gap = history.start_at.to_time - history.finish_at.to_time 
-    if(gap > history.job.schedule_min * 60)
+    #gap = history.start_at.to_time - history.finish_at.to_time 
+    #if(gap > history.job.schedule_min * 60)
       #Rails.logger.info "job 처리지연: #{history.job.name}"
-    end
+    #end
   end
 end
